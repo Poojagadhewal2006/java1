5a3d0e98deff501d792c2b7c28f6aec17fba1d15
unknown <poojagadhewal2006gmail.com>
2025-04-21 14:04:51 +0530

    ABC

--- BasicString.java
+++ BasicString.java
@@ -1,8 +1,12 @@
 public class BasicString {
     public static void main(String[] args) {
         String str ="Hello";
-        System.out.println(str);
-        System.out.println(str.length());
-        System.out.println(str.charAt( 1));
+       // System.out.println(str);
+       // System.out.println(str.length());
+       // System.out.println(str.charAt( 1));
+        for(int i=0;i<str.length();i++)
+        {
+            System.out.println(str.charAt(i));
+        }
     }
 }
