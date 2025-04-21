497e287f406f7e4483f28b50f4ad5f5fa4c51c40
unknown <poojagadhewal2006gmail.com>
2025-04-21 13:57:44 +0530

    abc

---
+++ BasicString.java
@@ -0,0 +1,8 @@
+public class BasicString {
+    public static void main(String[] args) {
+        String str ="Hello";
+        System.out.println(str);
+        System.out.println(str.length());
+        System.out.println(str.charAt( 1));
+    }
+}
--- ConvertSingleArray.java
+++ ConvertSingleArray.java
@@ -1,19 +1,33 @@
 public class ConvertSingleArray {
     public static void main(String[] args) {
-        int [] [] arr = { {1,2,3,4},{3,4,5,6},{4,5,6,7}};
+        int [] [] arr = {{1,2,3,4},{3,4,5,6},{4,5,6,7}};
         int totalcount=0;
-         for(int i=0;i<arr.length;i++)
-        {
+       /*   for(int i=0;i<arr.length;i++)
+         {
             for(int j=0;j<arr[i].length;j++)
             {
-                System.out.println(totalcount);
+                System.out.println("Hi");
                 totalcount++;
             }
-        }
-      /*   for(int i=0;i<arr.length;i++)
+         }*/
+        for(int i=0;i<arr.length;i++)
         {
             totalcount = totalcount + arr[i].length;
         }
-        System.out.println(totalcount);*/
+        System.out.println(totalcount);
+        int outArr [] = new  int[totalcount];
+        int k = 0;
+        for(int x=0;x<arr.length;x++)
+        {
+            for(int y=0;y<arr[x].length;y++)
+            {
+              outArr[k] = arr[x][y];
+              k++;
+            }
+        }
+        for(int s=0;s<totalcount;s++)
+        {
+            System.out.println(outArr[s] + " ");
+        }
     }
 }
