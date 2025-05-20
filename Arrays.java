57e52e2a50d60a7c89a3e3e41245cb59c313ddd7
unknown <poojagadhewal2006gmail.com>
2025-05-20 10:54:39 +0530

    java1

---
+++ Arrays.java
@@ -0,0 +1,33 @@
+public class Arrays {
+   public static void main(String[] args) {
+      
+   int arr[] = {-4,0,5,5,0,0,-5};
+   int index=0;
+   int newArr[] = new int[arr.length];
+   for(int i=0;i<arr.length;i++)
+   {
+      if(arr[i] < 0)
+      {
+         newArr[index++] = arr[i];
+      }
+   }
+   
+   for(int j=0;j<arr.length;j++)
+   {
+      if(arr[j] == 0)
+      {
+         newArr[index++] =arr[j];
+      }
+   }
+   for(int k=0;k<arr.length;k++)
+   {
+      if(arr[k] == 0)
+      {
+         newArr[index++] = arr[k];
+      }
+   }
+   for(int p = arr){
+   System.out.println(p);
+   }
+}
+}
